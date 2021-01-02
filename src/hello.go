623b93daa236ee48ef9c433185e3041dba0e77fb
kim-hyunjin <hyunjin1612@gmail.com>
2021-01-03 01:19:46 +0900

    for문을 사용한 피보나치 수열

--- src/hello.go
+++ src/hello.go
@@ -9,5 +9,5 @@ import (
 func main() {
 	fmt.Println("hello, go")
 	fmt.Printf("총합: %d\n", functest.Sum2(10, 0))
-	fmt.Printf("피보나치: %d\n", functest.Fibonacci(10))
+	fmt.Printf("피보나치: %d\n", functest.Fibonacci2(10))
 }

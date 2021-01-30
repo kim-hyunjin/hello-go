899e0d6da2067bc69b26f796039e1c6a87800bda
Kim <hyunjin1612@gmail.com>
2021-01-30 23:41:21 +0900

    double linked list

--- datastructure/linkedlist.go
+++ datastructure/linkedlist.go
@@ -4,6 +4,7 @@ import "fmt"
 
 type Node struct {
 	next *Node
+	prev *Node // 더블 링크드 리스트
 	val  int
 }
 type LinkedList struct {
@@ -18,30 +19,33 @@ func (l *LinkedList) AddNode(val int) {
 		return
 	}
 	l.tail.next = &Node{val: val}
+	prev := l.tail
 	l.tail = l.tail.next
+	l.tail.prev = prev
 }
 
-func (l *LinkedList) RemoveNode(node *Node) {
-	if node == l.root {
+func (l *LinkedList) RemoveNode(target *Node) {
+	if target == l.root {
 		l.root = l.root.next
-		if l.root == nil {
-			l.tail = nil
-		}
+		l.root.prev = nil
+		target = nil
 		return
 	}
 
-	// 지울 노드의 이전 노드 찾기
-	prev := l.root
-	for prev.next != node {
-		prev = prev.next
-	}
+	// 지울 노드의 이전 노드 찾기 => 더블링크드리스트로 만들어서 prev를 찾는 부분이 필요없어짐.
+	// prev := l.root
+	// for prev.next != node {
+	// 	prev = prev.next
+	// }
 
-	if node == l.tail {
-		prev.next = nil
-		l.tail = prev
+	if target == l.tail {
+		l.tail = l.tail.prev
+		l.tail.next = nil
 	} else {
-		prev.next = node.next
+		target.prev.next = target.next
+		target.next.prev = target.prev
 	}
+	target = nil
 }
 
 func (l *LinkedList) PrintNode() {
@@ -49,6 +53,16 @@ func (l *LinkedList) PrintNode() {
 	for node.next != nil {
 		fmt.Printf("%d -> ", node.val)
 		node = node.next
+	}
+	fmt.Printf("%d\n", node.val)
+}
+
+func (l *LinkedList) PrintReverse() {
+	node := l.tail
+
+	for node.prev != nil {
+		fmt.Printf("%d -> ", node.val)
+		node = node.prev
 	}
 	fmt.Printf("%d\n", node.val)
 }
@@ -71,4 +85,5 @@ func LinkedListTest() {
 
 	list.RemoveNode(list.root.next)
 	list.PrintNode()
+	list.PrintReverse()
 }

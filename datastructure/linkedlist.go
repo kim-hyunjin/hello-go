186e426763f1c1e3ecf7f502aef9d0deacf93ed9
Kim HyunJin <hyunjin1612@gmail.com>
2021-02-01 13:40:01 +0900

    linked list로 stack과 queue 만들기

--- datastructure/linkedlist.go
+++ datastructure/linkedlist.go
@@ -27,17 +27,17 @@ func (l *LinkedList) AddNode(Val int) {
 func (l *LinkedList) RemoveNode(target *Node) {
 	if target == l.Root {
 		l.Root = l.Root.Next
-		l.Root.Prev = nil
+		if l.Root != nil {
+			l.Root.Prev = nil
+		}
 		target = nil
 		return
 	}
-
 	// 지울 노드의 이전 노드 찾기 => 더블링크드리스트로 만들어서 Prev를 찾는 부분이 필요없어짐.
 	// Prev := l.Root
 	// for Prev.Next != node {
 	// 	Prev = Prev.Next
 	// }
-
 	if target == l.Tail {
 		l.Tail = l.Tail.Prev
 		l.Tail.Next = nil
@@ -46,6 +46,38 @@ func (l *LinkedList) RemoveNode(target *Node) {
 		target.Next.Prev = target.Prev
 	}
 	target = nil
+}
+
+func (l *LinkedList) Back() int {
+	if l.Tail != nil {
+		return l.Tail.Val
+	}
+	return 0
+}
+
+func (l *LinkedList) PopBack() {
+	if l.Tail == nil {
+		return
+	}
+	l.RemoveNode(l.Tail)
+}
+
+func (l *LinkedList) Empty() bool {
+	return l.Root == nil
+}
+
+func (l *LinkedList) Front() int {
+	if l.Root != nil {
+		return l.Root.Val
+	}
+	return 0
+}
+
+func (l *LinkedList) PopFront() {
+	if l.Root == nil {
+		return
+	}
+	l.RemoveNode(l.Root)
 }
 
 func (l *LinkedList) PrintNode() {
--- hello.go
+++ hello.go
@@ -1,11 +1,68 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/kim-hyunjin/hello-go/datastructure"
 )
 
 func main() {
-	LinkedListTest()
+	stackTest()
+	queueTest()
+}
+
+func stackTest() {
+	stack := datastructure.NewStack()
+
+	for i := 1; i <= 5; i++ {
+		stack.Push(i)
+	}
+
+	fmt.Println("New Stack")
+
+	for !stack.Empty() {
+		val := stack.Pop()
+		fmt.Println(val)
+	}
+	// stack := []int{}
+	// for i := 1; i <= 5; i++ {
+	// 	stack = append(stack, i)
+	// }
+
+	// fmt.Println(stack)
+
+	// for len(stack) > 0 {
+	// 	var last int
+	// 	last, stack = stack[len(stack)-1], stack[:len(stack)-1]
+	// 	fmt.Println(last)
+	// }
+}
+
+func queueTest() {
+	queue := datastructure.NewQueue()
+
+	for i := 1; i <= 5; i++ {
+		queue.Push(i)
+	}
+
+	fmt.Println("New Queue")
+	for !queue.Empty() {
+		val := queue.Pop()
+		fmt.Println(val)
+	}
+
+	// queue := []int{}
+	// for i := 1; i <= 5; i++ {
+	// 	queue = append(queue, i)
+	// }
+
+	// fmt.Println(queue)
+
+	// for len(queue) > 0 {
+	// 	var front int
+	// 	front, queue = queue[0], queue[1:]
+	// 	fmt.Println(front)
+	// }
 }
 
 func LinkedListTest() {

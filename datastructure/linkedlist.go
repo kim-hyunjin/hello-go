e69ab3d98c2b585d78cd171ec3ab0a04788213f9
Kim HyunJin <hyunjin1612@gmail.com>
2021-02-01 13:04:34 +0900

    linked list 패키지로 분리

--- datastructure/linkedlist.go
+++ datastructure/linkedlist.go
@@ -3,87 +3,66 @@ package datastructure
 import "fmt"
 
 type Node struct {
-	next *Node
-	prev *Node // 더블 링크드 리스트
-	val  int
+	Next *Node
+	Prev *Node // 더블 링크드 리스트
+	Val  int
 }
 type LinkedList struct {
-	root *Node
-	tail *Node
+	Root *Node
+	Tail *Node
 }
 
-func (l *LinkedList) AddNode(val int) {
-	if l.root == nil {
-		l.root = &Node{val: val}
-		l.tail = l.root
+func (l *LinkedList) AddNode(Val int) {
+	if l.Root == nil {
+		l.Root = &Node{Val: Val}
+		l.Tail = l.Root
 		return
 	}
-	l.tail.next = &Node{val: val}
-	prev := l.tail
-	l.tail = l.tail.next
-	l.tail.prev = prev
+	l.Tail.Next = &Node{Val: Val}
+	Prev := l.Tail
+	l.Tail = l.Tail.Next
+	l.Tail.Prev = Prev
 }
 
 func (l *LinkedList) RemoveNode(target *Node) {
-	if target == l.root {
-		l.root = l.root.next
-		l.root.prev = nil
+	if target == l.Root {
+		l.Root = l.Root.Next
+		l.Root.Prev = nil
 		target = nil
 		return
 	}
 
-	// 지울 노드의 이전 노드 찾기 => 더블링크드리스트로 만들어서 prev를 찾는 부분이 필요없어짐.
-	// prev := l.root
-	// for prev.next != node {
-	// 	prev = prev.next
+	// 지울 노드의 이전 노드 찾기 => 더블링크드리스트로 만들어서 Prev를 찾는 부분이 필요없어짐.
+	// Prev := l.Root
+	// for Prev.Next != node {
+	// 	Prev = Prev.Next
 	// }
 
-	if target == l.tail {
-		l.tail = l.tail.prev
-		l.tail.next = nil
+	if target == l.Tail {
+		l.Tail = l.Tail.Prev
+		l.Tail.Next = nil
 	} else {
-		target.prev.next = target.next
-		target.next.prev = target.prev
+		target.Prev.Next = target.Next
+		target.Next.Prev = target.Prev
 	}
 	target = nil
 }
 
 func (l *LinkedList) PrintNode() {
-	node := l.root
-	for node.next != nil {
-		fmt.Printf("%d -> ", node.val)
-		node = node.next
+	node := l.Root
+	for node.Next != nil {
+		fmt.Printf("%d -> ", node.Val)
+		node = node.Next
 	}
-	fmt.Printf("%d\n", node.val)
+	fmt.Printf("%d\n", node.Val)
 }
 
 func (l *LinkedList) PrintReverse() {
-	node := l.tail
+	node := l.Tail
 
-	for node.prev != nil {
-		fmt.Printf("%d -> ", node.val)
-		node = node.prev
+	for node.Prev != nil {
+		fmt.Printf("%d -> ", node.Val)
+		node = node.Prev
 	}
-	fmt.Printf("%d\n", node.val)
-}
-
-func LinkedListTest() {
-	list := &LinkedList{}
-	list.AddNode(0)
-
-	for i := 1; i < 10; i++ {
-		list.AddNode(i)
-	}
-
-	list.PrintNode()
-
-	list.RemoveNode(list.root)
-	list.PrintNode()
-
-	list.RemoveNode(list.tail)
-	list.PrintNode()
-
-	list.RemoveNode(list.root.next)
-	list.PrintNode()
-	list.PrintReverse()
+	fmt.Printf("%d\n", node.Val)
 }
--- hello.go
+++ hello.go
@@ -5,5 +5,26 @@ import (
 )
 
 func main() {
-	datastructure.LinkedListTest()
+	LinkedListTest()
+}
+
+func LinkedListTest() {
+	list := &datastructure.LinkedList{}
+	list.AddNode(0)
+
+	for i := 1; i < 10; i++ {
+		list.AddNode(i)
+	}
+
+	list.PrintNode()
+
+	list.RemoveNode(list.Root)
+	list.PrintNode()
+
+	list.RemoveNode(list.Tail)
+	list.PrintNode()
+
+	list.RemoveNode(list.Root.Next)
+	list.PrintNode()
+	list.PrintReverse()
 }

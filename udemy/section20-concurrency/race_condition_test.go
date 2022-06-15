2b9c3f7d15fb225664fa6954280e7ca7625f80cf
kim-hyunjin <hyunjin1612@gmail.com>
2022-06-15 23:26:17 +0900

    mutex, atomic package

--- udemy/section20-concurrency/race_condition_test.go
+++ udemy/section20-concurrency/race_condition_test.go
@@ -5,3 +5,11 @@ import "testing"
 func TestRaceCondition(t *testing.T) {
 	raceConditionExam()
 }
+
+func TestMutexExam(t *testing.T) {
+	mutexExam()
+}
+
+func TestAtomic(t *testing.T) {
+	atomicExam()
+}

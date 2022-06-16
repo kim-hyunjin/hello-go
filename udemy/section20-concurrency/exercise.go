a686d636d6dd82bf7adb9061988e8aa8b769bcf0
kim-hyunjin <hyunjin1612@gmail.com>
2022-06-16 23:32:46 +0900

    concurrency exercise1

---
+++ udemy/section20-concurrency/exercise.go
@@ -0,0 +1,33 @@
+package section20concurrency
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func exercise1() {
+
+	fmt.Println("begin cpu", runtime.NumCPU())
+	fmt.Println("begin gs", runtime.NumGoroutine())
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		fmt.Println("hello from thing one")
+		wg.Done()
+	}()
+
+	go func() {
+		fmt.Println("hello from thing two")
+		wg.Done()
+	}()
+
+	fmt.Println("mid cpu", runtime.NumCPU())
+	fmt.Println("mid gs", runtime.NumGoroutine())
+
+	wg.Wait()
+	fmt.Println("about to exit")
+	fmt.Println("end cpu", runtime.NumCPU())
+	fmt.Println("end gs", runtime.NumGoroutine())
+}
---
+++ udemy/section20-concurrency/exercise_test.go
@@ -0,0 +1,7 @@
+package section20concurrency
+
+import "testing"
+
+func TestExer1(t *testing.T) {
+	exercise1()
+}

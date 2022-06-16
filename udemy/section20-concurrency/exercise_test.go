5f5bb87658449cde13e220876fbf366f7f212567
kim-hyunjin <hyunjin1612@gmail.com>
2022-06-16 23:54:04 +0900

    more exercise

--- udemy/section20-concurrency/exercise_test.go
+++ udemy/section20-concurrency/exercise_test.go
@@ -5,3 +5,15 @@ import "testing"
 func TestExer1(t *testing.T) {
 	exercise1()
 }
+
+func TestExer2(t *testing.T) {
+	exercise2()
+}
+
+func TestExer3(t *testing.T) {
+	exercise3()
+}
+
+func TestExer4(t *testing.T) {
+	exercise4()
+}
